8866c01ba4a24780fcc105f093eb943e6074381b
Maria Vitoria <maria.suzarth@hotmail.com>
2024-05-17 10:31:01 -0300

    Correção do Forms
    Realizei a inclusão do CPF com um CharField com o tamanho de igual a 30. Foi realizado a implementação da validação da quantidade de caracteres para o campo CPF de acordo com o requisito

--- agenda/core/forms.py
+++ agenda/core/forms.py
@@ -4,6 +4,7 @@ from django.core.exceptions import ValidationError
 
 class AgendaForm(forms.Form):
     nome = forms.CharField(max_length=150)
+    cpf = forms.CharField(max_length=30)
     telefone = forms.CharField(max_length=20)
         
     def clean_nome(self):
@@ -12,6 +13,12 @@ class AgendaForm(forms.Form):
 
     def clean_telefone(self):
         telefone = self.cleaned_data['telefone']
-        if telefone.startswith('19'):
+        if telefone.startswith('19') or telefone.startswith('16'):
             return telefone
-        raise ValidationError('DDD válido somente o 19')
+        raise ValidationError('DDD válido somente o 19 e 16')
+    
+    def clean_cpf(self):
+        cpf = self.cleaned_data['cpf']
+        if len(cpf) == 11:
+            return cpf
+        raise ValidationError('CPF deve conter exatamente 11 caracteres')
