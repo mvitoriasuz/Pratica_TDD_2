2ae2ffc0f7310b90de43e416d3ae17b64e1f5dfa
Maria Vitoria <maria.suzarth@hotmail.com>
2024-05-17 10:42:28 -0300

    Correção models.py
    Inclusão do Campo CPF no models. Incluí também o campo de modificado_em com Auto_now=True para atualizar automaticamente a data e hora quando um registro for atualizado.
    
    Modificado também o fuso horário para Brasília

--- agenda/core/models.py
+++ agenda/core/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class AgendaModel(models.Model):
     nome = models.CharField('Nome', max_length=150)
     telefone = models.CharField('Telefone', max_length=20)
+    cpf = models.CharField('CPF', max_length=30)
+    modificado_em = models.DateTimeField('Data de Modificação', auto_now=True)
 
     def __str__(self):
         return self.nome
